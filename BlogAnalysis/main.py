8fa404614900703bb744ee4fb8c41f67a2849dac
Emily Felker <emilyfelker@gmail.com>
2024-02-16 11:36:15 +0100

    Modified program to add persistent cashing to GPT calls

--- BlogAnalysis/main.py
+++ BlogAnalysis/main.py
@@ -1,10 +1,21 @@
-from data import get_blogpost_dataset
-from analysis import add_features_to_dataset, preview_features
-from plot import create_scatter_plot, plt
+from BlogAnalysis.data import get_blogpost_dataset
+from analysis import add_features_to_dataset, preview_features, show_summaries
+from plot import create_scatter_plot
 
 if __name__ == '__main__':
 
-    dataset = get_blogpost_dataset("XangaBlogPosts", 1)
+    dataset = get_blogpost_dataset("data/XangaBlogPosts")
     dataset_with_features = add_features_to_dataset(dataset)
+
+    # Print post title, content preview (first 80 characters of post body), and features for each blog post
     preview_features(dataset_with_features)
-    #figure, plt = create_scatter_plot(dataset_with_features, "real_data_graphed.png", 300)
+
+    # Print just the ~5-word topic summary that ChatGPT generated for each blog post
+    show_summaries(dataset_with_features)
+
+    # Create graph of some feature against my age/over time
+    create_scatter_plot(dataset_with_features,
+                        y_variable="age_estimate",
+                        title="Actual Age vs. GPT Estimate",
+                        filename="output/real_data_graphed.png",
+                        dpi=300)
